dc394838547c68ad3d21ff0c302475f0602a1ac2
sj <songjian@codeorder.cn>
2023-07-19 17:36:15 +0800

    升级eastmoeny包

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request  # Importing the render_template function from Flask module to render HTML templates.
-import sys
-sys.path.append('/home/sj/code/quant/economic-data-tools/eastmoney')
-from eastmoney import F10
+from eastmoney.f10 import shareholder_research
 
 app = Flask(__name__)
 
@@ -18,7 +16,6 @@ def sjkzr():
     """
     code=request.args.get('code', default='SH600161')
     date=request.args.get('date', default='2022-03-31')
-    a=F10()
-    b=a.shareholder_research(code=code, date=date)
+    b=shareholder_research(code=code, date=date)
     # holders = b['sjkzr'][0]['HOLDER_NAME']
     return render_template('stock.html', holders=b['sjkzr'][0])
